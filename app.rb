6f89c547b4470df4665e7e949a950aed8897a2bd
yeah-right <claar.zack@gmail.com>
2019-04-02 14:49:38 -0600

    All tests passing

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    translated_phrase = PigLatinizer.new
+    @pl = translated_phrase.piglatinize(params[:user_phrase])
+    erb :results
+  end
 end
